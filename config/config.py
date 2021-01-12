6b6198dfe6ce0c2fe8c5674a091afb1ee7952f90
thanhnv <nguyenthanh2303@gmail.com>
2021-01-12 17:11:17 +0700

    change max batch size to 100

--- config/config.py
+++ config/config.py
@@ -9,7 +9,7 @@ class Config:
 class Sawtooth_Config:
     REST_API = 'http://localhost:8008'
     VALIDATOR_TCP = 'tcp://localhost:4004'
-    MAX_BATCH_SIZE = 1000
+    MAX_BATCH_SIZE = 100
 
 
 class ElasticSearchConfig:
