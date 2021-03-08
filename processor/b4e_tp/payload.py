ef21f8cb761107f3c6f51b34102e6242a5fc2c65
thanhnv <nguyenthanh2303@gmail.com>
2021-03-08 13:40:35 +0700

    payload protobuf

--- processor/b4e_tp/payload.py
+++ processor/b4e_tp/payload.py
@@ -30,26 +30,34 @@ class B4EPayload(object):
 
     @property
     def data(self):
+        if self._transaction.HasField('create_actor') and \
+                self._transaction.action == \
+                payload_pb2.B4EPayload.CREATE_ACTOR:
+            return self._transaction.create_institution
         if self._transaction.HasField('create_institution') and \
                 self._transaction.action == \
                 payload_pb2.B4EPayload.CREATE_INSTITUTION:
-            return self._transaction.create_institution
+            return self._transaction.create_teacher
         if self._transaction.HasField('create_teacher') and \
                 self._transaction.action == \
                 payload_pb2.B4EPayload.CREATE_TEACHER:
-            return self._transaction.create_teacher
-        if self._transaction.HasField('create_edu_officer') and \
-                self._transaction.action == \
-                payload_pb2.B4EPayload.CREATE_EDU_OFFICER:
             return self._transaction.create_edu_officer
-        if self._transaction.HasField('create_vote') and \
+        if self._transaction.HasField('create_portfolio') and \
                 self._transaction.action == \
-                payload_pb2.B4EPayload.CREATE_VOTE:
-            return self._transaction.create_vote
+                payload_pb2.B4EPayload.CREATE_PORTFOLIO:
+            return self._transaction.create_portfolio
         if self._transaction.HasField('create_class') and \
                 self._transaction.action == \
                 payload_pb2.B4EPayload.CREATE_CLASS:
             return self._transaction.create_class
+        if self._transaction.HasField('create_voting') and \
+                self._transaction.action == \
+                payload_pb2.B4EPayload.CREATE_VOTING:
+            return self._transaction.create_voting
+        if self._transaction.HasField('create_vote') and \
+                self._transaction.action == \
+                payload_pb2.B4EPayload.VOTE:
+            return self._transaction.vote
         if self._transaction.HasField('create_record') and \
                 self._transaction.action == \
                 payload_pb2.B4EPayload.CREATE_RECORD:
@@ -66,10 +74,34 @@ class B4EPayload(object):
                 self._transaction.action == \
                 payload_pb2.B4EPayload.UPDATE_RECORD:
             return self._transaction.update_record
-        if self._transaction.HasField('update_actor_info') and \
+        if self._transaction.HasField('modify_subject') and \
                 self._transaction.action == \
-                payload_pb2.B4EPayload.UPDATE_ACTOR_INFO:
-            return self._transaction.update_actor_info
+                payload_pb2.B4EPayload.MODIFY_SUBJECT:
+            return self._transaction.modify_subject
+        if self._transaction.HasField('modify_cert') and \
+                self._transaction.action == \
+                payload_pb2.B4EPayload.MODIFY_CERT:
+            return self._transaction.modify_cert
+        if self._transaction.HasField('revoke_cert') and \
+                self._transaction.action == \
+                payload_pb2.B4EPayload.REVOKE_CERT:
+            return self._transaction.revoke_cert
+        if self._transaction.HasField('reactive_cert') and \
+                self._transaction.action == \
+                payload_pb2.B4EPayload.REACTIVE_CERT:
+            return self._transaction.reactive_cert
+        if self._transaction.HasField('update_actor_profile') and \
+                self._transaction.action == \
+                payload_pb2.B4EPayload.UPDATE_ACTOR_PROFILE:
+            return self._transaction.update_actor_profile
+        if self._transaction.HasField('reject_institution') and \
+                self._transaction.action == \
+                payload_pb2.B4EPayload.REJECT_INSTITUTION:
+            return self._transaction.reject_institution
+        if self._transaction.HasField('active_institution') and \
+                self._transaction.action == \
+                payload_pb2.B4EPayload.ACTIVE_INSTITUTION:
+            return self._transaction.active_institution
         if self._transaction.HasField('set_b4e_environment') and \
                 self._transaction.action == \
                 payload_pb2.B4EPayload.SET_B4E_ENVIRONMENT:
